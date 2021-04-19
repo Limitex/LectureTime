2ac26dec338b13af22cb7176edd1569e913da500
Limitex <76650151+Limitex@users.noreply.github.com>
2021-04-19 19:44:14 +0900

    FirstCommit

---
+++ LectureTime/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace LectureTime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NowTimeFormLabel = new System.Windows.Forms.Label();
+            this.NowTimeStatusLabel = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // NowTimeFormLabel
+            // 
+            this.NowTimeFormLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.NowTimeFormLabel.Font = new System.Drawing.Font("Consolas", 140.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NowTimeFormLabel.ForeColor = System.Drawing.Color.White;
+            this.NowTimeFormLabel.Location = new System.Drawing.Point(12, 127);
+            this.NowTimeFormLabel.Name = "NowTimeFormLabel";
+            this.NowTimeFormLabel.Size = new System.Drawing.Size(960, 239);
+            this.NowTimeFormLabel.TabIndex = 0;
+            this.NowTimeFormLabel.Text = "00:00:00";
+            this.NowTimeFormLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // NowTimeStatusLabel
+            // 
+            this.NowTimeStatusLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.NowTimeStatusLabel.Font = new System.Drawing.Font("Consolas", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NowTimeStatusLabel.ForeColor = System.Drawing.Color.White;
+            this.NowTimeStatusLabel.Location = new System.Drawing.Point(12, 366);
+            this.NowTimeStatusLabel.Name = "NowTimeStatusLabel";
+            this.NowTimeStatusLabel.Size = new System.Drawing.Size(960, 95);
+            this.NowTimeStatusLabel.TabIndex = 0;
+            this.NowTimeStatusLabel.Text = "None";
+            this.NowTimeStatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.ForeColor = System.Drawing.Color.Red;
+            this.progressBar1.Location = new System.Drawing.Point(12, 534);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(960, 15);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.NowTimeStatusLabel);
+            this.Controls.Add(this.NowTimeFormLabel);
+            this.MinimumSize = new System.Drawing.Size(1000, 600);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormClosing_event);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label NowTimeFormLabel;
+        private System.Windows.Forms.Label NowTimeStatusLabel;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ LectureTime/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//
+// TIMETABLE 定数で時間割の最大値を設定する
+// startTime授業開始時間
+// endedTime授業狩猟時間
+// それぞれ上から順に記述していく
+//
+
+namespace LectureTime
+{
+    public partial class Form1 : Form
+    {
+        public const int MAX_TIMETABLE = 6;
+        public static string[] StartTime =
+        {
+            "8:30:00",
+            "10:10:00",
+            "13:10:00",
+            "14:50:00",
+            "16:30:00",
+            "18:10:00"
+        };
+        public static string[] EndedTime =
+        {
+            "10:00:00",
+            "11:40:00",
+            "14:40:00",
+            "16:20:00",
+            "18:00:00",
+            "19:40:00",
+        };
+
+        public int[] StartTimeValue = new int[MAX_TIMETABLE];
+        public int[] EndedTimeValue = new int[MAX_TIMETABLE];
+
+        int PeriodNumber = -1;
+        Thread eventHandleThread;
+        delegate void d();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            eventHandleThread = new Thread(() =>
+            {
+                DateTime dt, dtbf = DateTime.Now;
+                while (true)
+                {
+                    dt = DateTime.Now;
+                    if (dt != dtbf)
+                    {
+                        Invoke(new d(everySeconds_event));
+                        dtbf = dt;
+                    }
+                    Thread.Sleep(1);
+                }
+            });
+
+            for (int i = 0; i < MAX_TIMETABLE; i++)
+            {
+                StartTimeValue[i] = ConvertToSeconds(StartTime[i]);
+                EndedTimeValue[i] = ConvertToSeconds(EndedTime[i]);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            eventHandleThread.Start();
+        }
+
+        private void FormClosing_event(object sender, FormClosingEventArgs e)
+        {
+            eventHandleThread.Abort();
+        }
+
+        private void everySeconds_event()
+        {
+            DateTime dt = DateTime.Now;
+            string dtstr = dt.ToString("HH:mm:ss"), StatusLabelStr;
+            int ConvertedSeconds = ConvertToSeconds(dtstr);
+            PeriodNumber = CheckPeriodNumber(dt);
+
+            if (PeriodNumber != -1)
+            {
+                progressBar1.Minimum = StartTimeValue[PeriodNumber];
+                progressBar1.Maximum = EndedTimeValue[PeriodNumber];
+                progressBar1.Value = ConvertedSeconds;
+                StatusLabelStr = "During the " + (PeriodNumber + 1).ToString() + " time period class!";
+                progressBar1.BackColor = Color.Black;
+            }
+            else
+            {
+                progressBar1.Minimum = 0;
+                progressBar1.Maximum = 0;
+                progressBar1.Value = 0;
+                StatusLabelStr = "Time without class!";
+                progressBar1.BackColor = Color.Blue;
+            }
+            NowTimeStatusLabel.Text = StatusLabelStr;
+            NowTimeFormLabel.Text = dtstr;
+        }
+
+        private int ConvertToSeconds(string str)
+        {
+            string[] rawTime = str.Split(':');
+            double allTime = 0;
+            for (int i = 0; i < rawTime.Length; i++)
+                allTime += int.Parse(rawTime[i]) * (i == 2 ? 1 : Math.Pow(60, 2 - i));
+            return (int)allTime;
+        }
+
+        public int CheckPeriodNumber(DateTime dt)
+        {
+            int buf = -1;
+            for (int i = 0; i < MAX_TIMETABLE; i++)
+            {
+                int j = ConvertToSeconds(dt.ToString("HH:mm:ss"));
+                if (StartTimeValue[i] <= j && j <= EndedTimeValue[i])
+                {
+                    buf = i;
+                    break;
+                }
+            }
+            return buf;
+        }
+    }
+}
